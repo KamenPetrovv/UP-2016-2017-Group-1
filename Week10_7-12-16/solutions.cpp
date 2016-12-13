196267124e583ef4f7fb2f01600ec01073e90f9f
Bozhin Katsarski <bozhin@gmail.com>
2016-12-13 13:04:10 +0200

    Fix folder names, fix week10 solutions format

--- Week10_7-12-16/solutions.cpp
+++ Week10_7-12-16/solutions.cpp
@@ -1,33 +1,29 @@
 #include<iostream>
 using namespace std;
 
-int fact(int n)
-{
-    if ( n == 1) return 1;
+int fact(int n) {
+    if ( n == 1)
+        return 1;
+
     cout<< n << "*" << "fact(" << n - 1<< ")" <<endl;
+
     return n * fact(n - 1);
 }
 
-long fibboRec(int n)
-{
+long fibboRec(int n) {
     if(n == 0 || n == 1)
-    {
         return 1;
-    }
+
     else
-    {
         return fibboRec(n-1) + fibboRec(n - 2);
-    }
 }
-void getFibboNums (long n, long arr[])
-{
+
+void getFibboNums (long n, long arr[]) { ///fill the arr[] array with the first n fibonacci numbers
     arr[0] = 1;
     arr[1] = 1;
-    for(long i = 2; i < n; i ++)
-    {
-        arr[i] = arr[i-1] + arr[i - 2];
-    }
 
+    for(long i = 2; i < n; i ++)
+        arr[i] = arr[i-1] + arr[i - 2];
 }
 
 void bubbleSort(long a[100], long length) {
@@ -61,17 +57,10 @@ void selectionSort(long a[], long length) {
 }
 
 int main() {
-    int b = fact(1);
-    cout << b <<endl;
-    cout << fibboRec(40);
-    long fibbo[100];
-    getFibboNums(100, fibbo) ;
-    for(long i = 0; i < 100; i ++)
-    {
-        cout << fibbo[i] <<endl;
-    }
 
-    ///testing
+    ///insert some code here to initilize whatever variables are needed and test the functions we wrote^
+
+    ///testing the sorting algorithms
     long a[10] = {1, 0, 2, 9, 3, 8, 4, 7, 5, 6};
     selectionSort(a, 10);
 
